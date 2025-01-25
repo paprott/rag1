2bfde3519917be7d97cd8a58e8c27dd2c1a3c53b
Tomasz Mroz <tsmroz@interia.pl>
2025-01-25 17:20:38 +0100

    update

--- song_excel_to_txt.py
+++ song_excel_to_txt.py
@@ -1,32 +1,40 @@
 import openpyxl
 import re
 
-# Read the Excel file
-workbook = openpyxl.load_workbook('Piosenki.xlsx')
-worksheet = workbook.active
 
-# Iterate through rows, skipping headers
-for row in worksheet.iter_rows(values_only=False):
-    if row[1] == 'autor' or row[2] == 'tytul' or row[3] == 'tekst':
-        continue  # Skip header row
+def main_loop():
+    # Read the Excel file
+    workbook = openpyxl.load_workbook('Piosenki.xlsx')
+    worksheet = workbook.active
 
-    author = str(row[1].value).strip()
-    title = str(row[2].value).strip()
-    text = str(row[3].value).strip()
+    # Iterate through rows, skipping headers
+    for row in worksheet.iter_rows(values_only=False):
+        if row[1] == 'autor' or row[2] == 'tytul' or row[3] == 'tekst':
+            continue  # Skip header row
 
-    # Generate filename from title
-    filename = "songs/" + title.strip() + '.txt'
+        author = str(row[1].value).strip()
+        title = str(row[2].value).strip()
+        text = str(row[3].value).strip()
 
-    with open(filename, 'w') as f:
-        temp = title + "\n"
-        temp = temp + author + "\n"
+        # Generate filename from title
+        filename = "songs/" + title.strip() + '.txt'
+
+        with open(filename, 'w') as f:
+            temp = title + "\n"
+            temp = temp + author + "\n"
+            
+            res=re.findall(r"https:.*$", text)
+            if len(res)>0:
+                res = res[0]
+            else:
+                res="https://"
+            
+            temp = temp + res +"\n\n"
+            temp = temp+ text + "\n"
+            f.write(f"{temp}")
+      
         
-        res=re.findall(r"https:.*$", text)
-        if len(res)>0:
-            res = res[0]
-        else:
-            res="https://"
-        
-        temp = temp + res +"\n\n"
-        temp = temp+ text + "\n"
-        f.write(f"{temp}")
+# Run the main loop
+if __name__ == "__main__":
+    main_loop()
+    
